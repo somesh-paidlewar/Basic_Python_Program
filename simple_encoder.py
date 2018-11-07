ee7d7bfb1c8526c5601fe0bc4f2ca264576d9783
somesh-paidlewar <44520291+somesh-paidlewar@users.noreply.github.com>
2018-11-07 18:56:46 +0530

    Update simple_encoder.py

--- simple_encoder.py
+++ simple_encoder.py
@@ -2,7 +2,7 @@
 # this program takes a string input message from the user 
 # and copies the encoded message to the clipboard of the user
 
-def main():
+def encoder():
 	import pyperclip
 	print("Enter your message to encode ")
 	message = input("Your message :- ")
@@ -13,4 +13,4 @@ def main():
 	print("Your message Succesfully encoded and copied to ")
 	print("your clipboard!!")
 
-main()
+encoder()
